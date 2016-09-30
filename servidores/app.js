05b54c36f42ddc4c9090b49ca44e20a02db03ae9
jose8club <jose8club@gmail.com>
2016-09-30 11:41:21 -0300

    comentarios en la rama

--- servidores/app.js
+++ servidores/app.js
@@ -1,3 +1,5 @@
+//variables objetos de la dependencia
+//server: servidor io:socket
 var express = require("express"),
 	app = express(),
 	server = require('http').createServer(app),
@@ -5,10 +7,16 @@ var express = require("express"),
 
 server.listen(3000);
 
+//1)direccion del servidor req: require, res: respuesta
+//cuando se acceda al index se renderize al sgte archivo
 app.get("/", function(req,res) {
 	res.sendFile(__dirname + '/index.html')
 });
 
+//3)genera la conexion al socket luego establecido
+//recibira el mensaje segun el nombre que le pusimos en 2) que
+//es 'send message' y despues recbibe el mensaje en 'data' y lo emitimos
+//a las demas personas con el nombre 'new message'
 io.sockets.on('connection', function(socket) {
 	socket.on('send message', function(data) {
 		io.sockets.emit('new message', data);
