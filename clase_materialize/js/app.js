9dea683fa6fb9902e70e43b812137831f815ae93
jose8club <jose8club@gmail.com>
2016-10-05 12:04:38 -0300

    Funcionamiento

--- clase_materialize/js/app.js
+++ clase_materialize/js/app.js
@@ -27,18 +27,6 @@ $(document).ready(function(){
 			//y el numero de columnas es el total= 12 dividido por el numero que es 3 por lo q habra 4 elementos por fila
 			for(var i=0;i<data.length;i++){
 
-				php=php + '<div class="col s3 m3 l3">'+
-							'<div class="card">'+			
-    							'<div class="card-image waves-effect waves-block waves-light">'+
-     								'<img class="activator" src="'+data[i].img+'">'+
-    							'</div>'+
-    							'<div class="card-content">'+
-      								'<span class="card-title activator grey-text text-darken-4"><h4><b><em>'+data[i].first_name+' '+data[i].last_name+'</em></b></h4><i class="material-icons right">Desplegar Comentario</i></span>'+
-      								'<p><a href="'+data[i].img+'"><em>Este es un enlace a la imagen puesta</em></a></p>'+
-    							'</div>'+
-    							'<div class="card-reveal">'+
-      								'<span class="card-title grey-text text-darken-4"><h4><b><em>'+data[i].first_name+' '+data[i].last_name+'</em></b></h4><i class="material-icons right">Contraer Comentario</i></span>'+
-
 				php=php + '<div id="'+data[i]._id+'" class="col s4 m4 l4 col">'+
 							'<div class="card" id="elem">'+			
     							'<div class="card-image waves-effect waves-block waves-light">'+
